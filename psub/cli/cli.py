c980f519d81e4594dca74add48e7fd1436452c21
Spencer Burke <info.spencer.burke@gmail.com>
2021-01-10 14:15:49 -0800

    Working on the functions which will handle loading the configuration information for the cli.

--- psub/cli/cli.py
+++ psub/cli/cli.py
@@ -3,17 +3,41 @@
     # get conf options from file and then return dictionary
 import toml
 import click
-import os.path
+from pathlib import Path
 
 def resolve_conf_path():
     """
     Returns:
-        str: the string representing the path to the configuration file
+        str: The string representing the path to the configuration file
     """
-    result = ""
+    conf_path_modern = '~/.config/.psub.toml'
+    conf_path_traditional = '~/.psub.toml'
+    conf_path_absolute = '/etc/psub.toml'
+
     # check for the file in .config
-     
+    path = Path(conf_path_modern) 
+    if (path.exists()):
+        return conf_path_modern
     # check for the file in ~
+    path = Path(conf_path_traditional)
+    elif (path.exists()):
+        return conf_path_traditional
     # revert to /etc conf file
+    path = Path(conf_path_absolute)
+    elif (path.exists()):
+        return conf_path_absolute
+    else:
+        return 'NAN'
+
+def get_conf(path):
+    """
+    Args:
+        path (str): The location of the configuration file in the file system
+
+    Returns:
+        dict: A dictionary containing all of the configuration parameters
+    """
+    
+    pass
 
 
