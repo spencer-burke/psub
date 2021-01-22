46f6206db464c14df0b3331fa778adfface892c5
Spencer Burke <info.spencer.burke@gmail.com>
2021-01-21 17:26:33 -0800

    Adding a small amount to the test file while attempting to understand the inner workings of the "work_on()" function. Currently attempting to add authetication credentials in a reasonably secure method unless a way is found to add input from stdin.
    On branch main
    
    Changes to be committed:
        modified:   tests/behaviour_work_on.py

--- tests/behaviour_work_on.py
+++ tests/behaviour_work_on.py
@@ -1,7 +1,23 @@
 import pytest
-import psub.client.Client
-from pathlib import toml
+from psub import client
+from pathlib import Path
+import toml
+import getpass
 
 def run_behaviour_work_on():
+    # create client object
     client = psub.client.Client()
+
+    # test getting the information from the terminal while inside a pytest
+    username = input('username: ')
+    password = getpass.getpass('password: ')
+
+    # authenticate and then call work_on 
+
+    # test what work_on returns
+"""
+This is under development. 
+I am currently looking for ways to get the password and usrname in a secure method
+As pytest does really allow for input from stdin.
+"""
 
