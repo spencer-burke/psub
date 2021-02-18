258b311850cacd0914dc0e43f4b2cb2603d54f9a
Spencer Burke <info.spencer.burke@gmail.com>
2021-02-18 13:48:25 -0800

    Adding minor changes to the "exec.py" file. Since the "get_headers()" function was created using that to get the request headers to interact with pwn_college. On branch main Changes to be committed:     modified:   exec.py

--- psub/exec.py
+++ psub/exec.py
@@ -28,15 +28,17 @@ def main():
     password = getpass.getpass('password: ')
 
     conf = cli.get_conf()
+# temporaririly adding exta headers variable as I don't know what to do with the whole get_conf() function
+    headers = cli.get_headers()
     pwn_session = requests.session()
     pwn_url = "https://cse466.pwn.college"
 
     client.login(pwn_url, pwn_session, username, password)
 
     # use the config information with the session info to make the request
-    client.work_on_chal(pwn_url, pwn_session, 101, conf)
+    client.work_on_chal(pwn_url, pwn_session, 101, headers)
     # attempt to submit a flag
-    client.submit_flag(pwn_url, pwn_session, "pwn_college{MK9noUdT-C1gNN5Spyd6IqDUexB.dFDMxwCN4UzW}", conf)
+    client.submit_flag(pwn_url, pwn_session, "pwn_college{MK9noUdT-C1gNN5Spyd6IqDUexB.dFDMxwCN4UzW}", headers)
     
 if __name__ == '__main__':
     main()
