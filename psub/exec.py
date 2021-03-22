7bd7b2bf979e002d5f8cee192634f7daf6b88879
Spencer Burke <info.spencer.burke@gmail.com>
2021-03-21 21:10:19 -0700

    Adding a small change which is just removing the "old_main" function from exec.py.

--- psub/exec.py
+++ psub/exec.py
@@ -5,24 +5,6 @@ import getpass
 import re
 import client  
 import cli 
-#from client import Client 
-
-def old_main():
-    username = input('username: ')
-    password = getpass.getpass('password: ')
-
-    client = Client()
-    logged_in = client.login(username, password)
-
-    if not logged_in:
-        print('You username or password is incorrect', file=sys.stderr, flush=True)
-        exit(1)
-
-    for challenge in client.challenges():
-        print(challenge)
-    
-    client.work_on(101)
-    print(type(client.work_on(101)))
 
 def main():
     username = input('username: ')
